ec7435b727989f6ccb33d7929e6f4bed633e7f3c
harsh <rohillah697@gmail.com>
2026-02-23 10:53:58 +0530

    Add files via upload

---
+++ 22.js
@@ -0,0 +1,113 @@
+// hello()
+// function hello(){
+//     console.log(name);
+//     console.log("error");
+
+//     if (true){
+//         var name = "shyam";
+//         let age = "21";
+
+//         console.log(name);
+//         console.log(age);
+
+//     }
+
+//     console.log(name);
+//     console.log(age)
+// }
+
+
+
+
+
+
+// execution context 
+
+// console.log(one());
+// console.log("start");
+
+// function one(){
+//     console.log(name);
+//     var name="ram";
+
+//     three();
+//     console.log(name);
+//     return "shyam "
+// }
+
+// console.log("mid");
+
+// function three(){
+//     return "hello developers"
+// }
+// function two(){
+//     console.log(age);
+//     let age=21;
+//     console.log(three());
+//     console.log(age);
+// }
+// console.log(two());
+// console.log("end");
+// one()
+
+
+
+
+// timer 
+
+// console.log("start");
+
+// setTimeout(() => {
+//     console.log("mid");
+
+// }, 5000);
+
+// console.log("end");
+
+
+
+
+
+// event loop
+
+// console.log("start");
+
+// setInterval(()=>{
+//     three()
+// },10000)
+
+// function one(){
+//     console.log(name);
+//     var name ="love";
+
+//     return name;
+//     console.log(three());
+
+// }
+
+// setTimeout(() => {
+    
+// }, 3000);
+
+// function two(){
+//     console.log(age);
+//     var age = 25;
+//     console.log(three());
+
+//     return age;
+
+
+
+// }
+
+// function three(){
+//     console.log("baba tillu B.TECH from Kanpur:lakshmi misthan bhandar")
+
+// }
+    
+// console.log("mid");
+
+// setTimeout(() => {
+    
+// }, 5000);
+// console.log("end");
