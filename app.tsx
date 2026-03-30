ad4e3fbd5ae560284202e307b91bef9599ace2c2
harsh <rohillah697@gmail.com>
2026-03-30 09:57:04 +0530

    Add files via upload

---
+++ app.tsx
@@ -0,0 +1,19 @@
+import "./styles.css";
+
+export default function App() {   let name = "harsh";
+  let status = false;
+  let hbb = { name: "shu", id: [1, 2, 3] };
+  let add = () => "hello";
+  const styles = { backgroundcolor: "teal", color: "red" };
+  return (
+    
+    
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+      <p>{hbb.id[1]}</p>
+      <h3>{add()},</h3>
+      <img src="" />
+      
+  );
+  ]
